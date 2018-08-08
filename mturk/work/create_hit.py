959be9a5848be472656888cd3b188c98fa104794
Brian Lubars <blubars@gmail.com>
2018-08-08 11:26:03 -0600

    get results script, survey modification

--- mturk/work/create_hit.py
+++ mturk/work/create_hit.py
@@ -15,6 +15,11 @@ import csv
 #---------------------------------------------------------
 # GLOBALS
 #---------------------------------------------------------
+# file to save the output of hit creation status
+STATUS_FILE_OUT = "created_hits.csv"
+# file to read in HTMLQuestion form if that's how we're
+# creating HITs. 
+QUESTION_FILE_IN = "html-question-survey.xml"
 
 # CREDENTIALS NOTE:
 # before using, set credentials either using the AWS CLI, or ~/.aws/credentials
@@ -26,7 +31,7 @@ endpoint_url = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 
 region_name = 'us-east-1'
 
-TITLE = "Should an AI do this task?"
+TITLE = "Should an AI do this task? -- 5"
 DESCRIPTION = "Academic study about peoples' sentiments towards 'delegating' different kinds of tasks to an AI (artificial intelligence) versus to a person. You will consider one randomly-selected task (e.g., mowing a lawn), and provide your opinion in the form of a short survey. The survey will take approximately 5 minutes."
 KEYWORDS = "AI, research, delegation, automation"
 REWARD = "0.80"
@@ -45,6 +50,39 @@ client = boto3.client(
 # 2. create HIT Layout (template for HIT w/ placeholder for input variables
 # 3. create HITs, plug input data into layouts.
 
+qualifications = [
+# percentage of HITs approved
+    {
+        'QualificationTypeId': '000000000000000000L0',
+        'Comparator': 'GreaterThanOrEqualTo',
+        'IntegerValues': [
+            99,
+        ],
+        'ActionsGuarded': 'PreviewAndAccept'
+    },
+    # number of HITs done
+    {
+        'QualificationTypeId': '00000000000000000040',
+        'Comparator': 'GreaterThanOrEqualTo',
+        'IntegerValues': [
+            50,
+        ],
+        #'ActionsGuarded': 'PreviewAndAccept'
+        'ActionsGuarded': 'Accept'
+    },
+    # worker's location
+    {
+        'QualificationTypeId': '00000000000000000071',
+        'Comparator': 'EqualTo',
+        'LocaleValues': [
+            {
+                'Country': 'US',
+            },
+        ],
+        'ActionsGuarded': 'PreviewAndAccept'
+    },
+]
+
 def create_hit_type():
     hitType = client.create_hit_type(
         AutoApprovalDelayInSeconds=172800, # 60s * 60m * 24hr * 2d = 
@@ -53,45 +91,19 @@ def create_hit_type():
         Title=TITLE, 
         Keywords=KEYWORDS,
         Description=DESCRIPTION,
-        QualificationRequirements=[
-            # percentage of HITs approved
-            {
-                'QualificationTypeId': '000000000000000000L0',
-                'Comparator': 'GreaterThanOrEqualTo',
-                'IntegerValues': [
-                    99,
-                ],
-                'ActionsGuarded': 'PreviewAndAccept'
-            },
-            # number of HITs done
-            {
-                'QualificationTypeId': '00000000000000000040',
-                'Comparator': 'GreaterThanOrEqualTo',
-                'IntegerValues': [
-                    50,
-                ],
-                #'ActionsGuarded': 'PreviewAndAccept'
-                'ActionsGuarded': 'Accept'
-            },
-            # worker's location
-            {
-                'QualificationTypeId': '00000000000000000071',
-                'Comparator': 'EqualTo',
-                'LocaleValues': [
-                    {
-                        'Country': 'US',
-                    },
-                ],
-                'ActionsGuarded': 'PreviewAndAccept'
-            },
-        ],
+        QualificationRequirements=[],
     )
 
     print("HIT Type Created! ID: {}".format(hitType['HITTypeId']))
     print("----------------------------------------")
     return hitType['HITTypeId']
 
-def create_hit(index, hit_id, hit_params):
+def save_hit_create_response(response, annotation):
+    with open(STATUS_FILE_OUT, 'a', newline='') as outfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(response['HIT']['HITId'], response['HIT']['CreationTime'], annotation, response['HIT']['Title'], response['HIT']['HITStatus'], response['HIT']['MaxAssignments']
+        
+def create_hit_with_layout_id(index, hit_id, hit_params, layout_id):
     split_id = hit_params[0]['Value']
     print("[{}]: Creating HIT for split_id {}".format(index, split_id))
     # To avoid a fee, do not create a HIT with more than 10 assignments.
@@ -104,13 +116,36 @@ def create_hit(index, hit_id, hit_params):
         #UniqueRequestToken='string',
         #AssignmentReviewPolicy={
         #HITReviewPolicy={
-        HITLayoutId='3SZCR22QJPJGD2U6YLKDUXNF9VXOAK',
+        HITLayoutId=layout_id,  # '3SZCR22QJPJGD2U6YLKDUXNF9VXOAK',
         HITLayoutParameters=hit_params
     )
     print("Done. HITId: {}\nCreationTime: \t{}\nTitle: \t{}\nDescription: \t{}\nKeywords:  \t{}\nHITStatus: \t{}\nAssignments: \t{}".format(response['HIT']['HITId'], response['HIT']['CreationTime'], response['HIT']['Title'], response['HIT']['Description'], response['HIT']['Keywords'], response['HIT']['HITStatus'], response['HIT']['MaxAssignments']))
     #print(response)
     print("----------------------------------------")
 
+def create_hit_with_html_question(index, hit_id, hit_params):
+    split_id = hit_params[0]['Value']
+    print("[{}]: Creating HIT for split_id {}".format(index, split_id))
+    
+    # read the HTMLQuestion in from file
+    with open(QUESTION_FILE_IN, 'r') as qfile:
+        question = qfile.read()
+    
+        # To avoid a fee, do not create a HIT with more than 10 assignments.
+        response = client.create_hit_with_hit_type(
+            HITTypeId=hit_id,
+            MaxAssignments=2, # num times HIT can be accepted before unavailable
+            LifetimeInSeconds=86400, # REQ: time after which HIT not available: 1 day
+            RequesterAnnotation=split_id,
+            #UniqueRequestToken='string',
+            #AssignmentReviewPolicy={
+            #HITReviewPolicy={
+            Question=question
+        )
+        print("Done. HITId: {}\nCreationTime: \t{}\nTitle: \t{}\nDescription: \t{}\nKeywords:  \t{}\nHITStatus: \t{}\nAssignments: \t{}".format(response['HIT']['HITId'], response['HIT']['CreationTime'], response['HIT']['Title'], response['HIT']['Description'], response['HIT']['Keywords'], response['HIT']['HITStatus'], response['HIT']['MaxAssignments']))
+        #print(response)
+        print("----------------------------------------")
+    
 def main():
     # check if we passed an input file
     if len(sys.argv) < 2:
@@ -127,7 +162,7 @@ def main():
                 hit_params.append({'Name':key, 'Value':value})
             print(hit_params)
             # iterate through input data, create HIT for each one.
-            create_hit(i, hit_id, hit_params)
+            create_hit_with_layout_id(i, hit_id, hit_params, '3YNXD5PU8XNO5MSHQBXI07S8TP6X4N') #'3ZWZ4MEZJXXQD14D3Z8UW2LYD111FQ')
     
     
 if __name__ == "__main__":
---
+++ mturk/work/get_results.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+#---------------------------------------------------------
+# FILE DESCRIPTION:
+#---------------------------------------------------------
+# Download and save results from completed HIT assignments
+
+#---------------------------------------------------------
+# IMPORTS
+#---------------------------------------------------------
+import boto3
+import sys
+import os.path
+import csv
+import xml.etree.ElementTree as ET
+
+#---------------------------------------------------------
+# GLOBALS
+#---------------------------------------------------------
+CSV_OUT_FILE = "out.csv"
+HIT_TYPE_ID = '3YNXD5PU8XNO5MSHQBXI07S8TP6X4N'
+
+# CREDENTIALS NOTE:
+# before using, set credentials either using the AWS CLI, or ~/.aws/credentials
+
+# SANDBOX VS PRODUCTION
+endpoint_url = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+# Uncomment this line to use in production
+# endpoint_url = 'https://mturk-requester.us-east-1.amazonaws.com'
+
+region_name = 'us-east-1'
+
+client = boto3.client(
+    'mturk',
+    endpoint_url=endpoint_url,
+    region_name=region_name,
+)
+
+#---------------------------------------------------------
+# SCRIPT 
+#---------------------------------------------------------
+
+def process_assignment(csvwriter, HITId, annotation, assignment):
+    assign_id = assignment['AssignmentId']
+    result_row = [HITId, annotation, assignment['WorkerId'], assign_id]
+    root = ET.fromstring(assignment['Answer'])
+    namespaces = {'ns': 'http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionFormAnswers.xsd'}
+    #for q_num, elem in enumerate(root.iterfind("Answer")):
+    for q_num, elem in enumerate(root.findall("ns:Answer", namespaces)):
+        #print(elem)
+        q_id = elem.find("ns:QuestionIdentifier", namespaces).text
+        q_resp = elem.find("ns:FreeText", namespaces).text
+        print("  - [Q{}] {}: {}".format(q_num, q_id, q_resp))
+        result_row.append(q_resp)
+    csvwriter.writerow(result_row)
+    return
+    
+def process_hit(csvwriter, hit, i):
+    id = hit['HITId']
+    #status = hit['HITStatus']
+    annotation = hit['RequesterAnnotation']
+    #reviewStatus = hit['HITReviewStatus']
+    #assignments = (hit['NumberOfAssignmentsAvailable'], hit['NumberOfAssignmentsPending'], hit['NumberOfAssignmentsCompleted'])
+    print("=============================================")
+    print("PROCESSING HIT [{}]: {}".format(i, id))
+    #print("  status: \t{}".format(status))
+    print("  annotation: {}".format(annotation))
+    #print("  review status: {}".format(reviewStatus))
+    #print("  assign avail/pend/compl: {}/{}/{}".format(assignments[0], assignments[1], assigments[2]))
+    res = client.list_assignments_for_hit(HITId=id, AssignmentStatuses=['Submitted'])
+    for i, assign in enumerate(res['Assignments']):
+        print("  ---------------------------------------------")
+        print("  ASSIGN [{}] ID={}:".format(i, assign['AssignmentId']))
+        process_assignment(csvwriter, id, annotation, assign)
+    #print("---------------------------------------------")
+
+def main():
+    # check if we passed an input file
+    isblank = os.path.isfile(CSV_OUT_FILE)
+    with open(CSV_OUT_FILE, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        if not isblank:
+            writer.writerow(['HIT id', 'Annotation', 'Worker id', 'assign id', 'gender', 'gender-input', 'age', 'tech-level', 'edu-level', 'l-trust', 'l-process', 'l-values', 'l-accountable', 'l-uncertainty', 'l-impact', 'l-intrinsic', 'l-learning', 'l-important', 'l-effort', 'l-abilities', 'sanity-check', 'l-expertise', 'label'])
+        #res = client.list_reviewable_hits(HITTypeId=HIT_TYPE_ID, Status='Reviewable')
+        i = 0
+        res = client.list_reviewable_hits(Status='Reviewable')
+        while( res['NumResults'] > 0 ):
+            print("Found {} reviewable HITs".format(res['NumResults']))
+            for hit in res['HITs']:
+                i += 1
+                process_hit(writer, hit, i)
+            res = client.list_reviewable_hits(Status='Reviewable', NextToken=res['NextToken'])
+        print("\nDone! Processed {} HITs".format(i))
+    
+if __name__ == "__main__":
+    main()
